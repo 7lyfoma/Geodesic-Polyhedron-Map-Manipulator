e4d3cb248330c6fe02317711c552afe399ad838e
7lyfoma <7lyfoma@gmail.com>
2025-09-09 11:41:45 +0100

    update calling of javafx functions to be outside of main

--- geodesic_polyhedron_map_manipulator/src/main/java/io/github/sevenlyfoma/geodesic_polyhedron_map_manipulator/HelloFx.java
+++ geodesic_polyhedron_map_manipulator/src/main/java/io/github/sevenlyfoma/geodesic_polyhedron_map_manipulator/HelloFx.java
@@ -1,4 +1,4 @@
-package com.example;
+package io.github.sevenlyfoma.geodesic_polyhedron_map_manipulator;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -6,20 +6,24 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class HelloFX extends Application {
+public class HelloFx extends Application {
 
     @Override
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+
+
+        Label l = new Label("Hello, JavaFX Hello fx " + javafxVersion + ", running on Java " + javaVersion + ".");
+
         Scene scene = new Scene(new StackPane(l), 640, 480);
+        
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args){
         launch(args);
     }
-
+    
 }
--- geodesic_polyhedron_map_manipulator/src/main/java/io/github/sevenlyfoma/geodesic_polyhedron_map_manipulator/Main.java
+++ geodesic_polyhedron_map_manipulator/src/main/java/io/github/sevenlyfoma/geodesic_polyhedron_map_manipulator/Main.java
@@ -1,25 +1,10 @@
 package io.github.sevenlyfoma.geodesic_polyhedron_map_manipulator;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX Main fx " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
-        launch();
+
+        HelloFx.main(args);
     }
 
 }
